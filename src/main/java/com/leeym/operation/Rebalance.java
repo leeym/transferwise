5b824f2fdc6f9c44770c343e60b4baa36047cf7c
Yen-Ming Lee <leeym@leeym.com>
2020-07-01 17:13:16 -0700

    replace Runnable with Callable

--- src/main/java/com/leeym/operation/Rebalance.java
+++ src/main/java/com/leeym/operation/Rebalance.java
@@ -1,9 +1,48 @@
 package com.leeym.operation;
 
-public class Rebalance implements Runnable {
+import com.leeym.api.*;
+import com.leeym.common.CurrencyCode;
+import com.leeym.common.ProfileId;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+public class Rebalance implements Callable<String> {
+
+    private final BorderlessAccountsAPI borderlessAccountsAPI;
+    private final RatesAPI ratesAPI;
+    private final ProfileId profileId;
+
+    public Rebalance(BorderlessAccountsAPI borderlessAccountsAPI, RatesAPI ratesAPI, ProfileId profileId) {
+        this.borderlessAccountsAPI = borderlessAccountsAPI;
+        this.ratesAPI = ratesAPI;
+        this.profileId = profileId;
+    }
 
     @Override
-    public void run() {
-
+    public String call() {
+        List<BorderlessAccount> borderlessAccounts = borderlessAccountsAPI.getBorderlessAccounts(profileId);
+        BorderlessAccount borderlessAccount = borderlessAccounts.get(0);
+        Map<String, BigDecimal> rateToUSD = new TreeMap<>();
+        ratesAPI.getRates(new CurrencyCode("USD")).forEach(rate -> {
+            rateToUSD.put(rate.getSource(), rate.getRate());
+        });
+        List<Amount> amounts = borderlessAccount.getBalances().stream().map(Balance::getAmount).collect(Collectors.toList());
+        Map<Amount, Amount> map = new HashMap<>();
+        for (Amount sourceAmount : amounts) {
+            if (sourceAmount.getCurrency().equals("USD")) {
+                map.put(sourceAmount, sourceAmount);
+            } else {
+                BigDecimal sourceValue = sourceAmount.getValue();
+                BigDecimal rate = rateToUSD.get(sourceAmount.getCurrency());
+                BigDecimal targetValue = sourceValue.multiply(rate);
+                Amount targetAmount = new Amount("USD", targetValue);
+                map.put(sourceAmount, targetAmount);
+            }
+            System.err.println(Arrays.asList(sourceAmount, map.get(sourceAmount)));
+        }
+        return map.toString();
     }
 }
--- src/test/java/com/leeym/operation/RebalanceTest.java
+++ src/test/java/com/leeym/operation/RebalanceTest.java
@@ -1,42 +1,21 @@
 package com.leeym.operation;
 
-import com.leeym.api.*;
-import com.leeym.common.CurrencyCode;
+import com.leeym.api.BaseAPITest;
+import com.leeym.api.BorderlessAccountsAPI;
+import com.leeym.api.RatesAPI;
+import com.leeym.api.Stage;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 class RebalanceTest extends BaseAPITest {
 
     BorderlessAccountsAPI borderlessAccountsAPI = new BorderlessAccountsAPI(Stage.SANDBOX, REAL_SANDBOX_API_TOKEN);
     RatesAPI ratesAPI = new RatesAPI(Stage.SANDBOX, REAL_SANDBOX_API_TOKEN);
 
+    @Disabled
     @Test
     public void test1() {
-        List<BorderlessAccount> borderlessAccounts = borderlessAccountsAPI.getBorderlessAccounts(REAL_SANDBOX_PERSONAL_PROFILE_ID);
-        BorderlessAccount borderlessAccount = borderlessAccounts.get(0);
-        Map<String, BigDecimal> rateToUSD = new TreeMap<>();
-        ratesAPI.getRates(new CurrencyCode("USD")).forEach(rate -> {
-            rateToUSD.put(rate.getTarget(), rate.getRate());
-        });
-        List<Amount> amounts = borderlessAccount.getBalances().stream().map(Balance::getAmount).collect(Collectors.toList());
-        Map<Amount, Amount> map = new HashMap<>();
-        for (Amount sourceAmount : amounts) {
-            if (sourceAmount.getCurrency().equals("USD")) {
-                map.put(sourceAmount, sourceAmount);
-            } else {
-                BigDecimal sourceValue = sourceAmount.getValue();
-                BigDecimal rate = rateToUSD.get(sourceAmount.getCurrency());
-                BigDecimal targetValue = sourceValue.multiply(rate);
-                Amount targetAmount = new Amount("USD", targetValue);
-                map.put(sourceAmount, targetAmount);
-            }
-        }
-        System.err.println(map);
+        Rebalance rebalance = new Rebalance(borderlessAccountsAPI, ratesAPI, REAL_SANDBOX_PERSONAL_PROFILE_ID);
+        rebalance.run();
     }
 }
