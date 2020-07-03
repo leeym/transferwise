f3999767ad0a129db9b3c3ca267cbc16dd1ba7ac
Yen-Ming Lee <leeym@leeym.com>
2020-07-03 06:40:46 -0700

    replace type String with Currency for currency

--- src/test/java/com/leeym/api/BorderlessAccountsApiITCase.java
+++ src/test/java/com/leeym/api/BorderlessAccountsApiITCase.java
@@ -6,6 +6,7 @@ import com.leeym.api.borderlessaccounts.CurrencyPairs;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Currency;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -24,7 +25,7 @@ class BorderlessAccountsApiITCase extends BaseApiITCase {
     @Test
     public void testCurrencyPairs() {
         CurrencyPairs currencyPairs = api.getCurrencyPairs();
-        Map<String, Set<String>> map = new HashMap<>();
+        Map<Currency, Set<Currency>> map = new HashMap<>();
         for (CurrencyPairs.SourceCurrency sourceCurrency : currencyPairs.sourceCurrencies) {
             for (CurrencyPairs.TargetCurrency targetCurrency : sourceCurrency.targetCurrencies) {
                 map.computeIfAbsent(sourceCurrency.currencyCode, k -> new HashSet<>()).add(targetCurrency.currencyCode);
