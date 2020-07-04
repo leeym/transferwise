4e70a61b836dda5fbf140e0f9f15d47a5f1e2b95
Yen-Ming Lee <leeym@leeym.com>
2020-07-03 22:08:04 -0700

    add BankDetailsId BorderlessAcountId BaseUrl

---
+++ src/main/java/com/leeym/api/borderlessaccounts/BankDetailsId.java
@@ -0,0 +1,28 @@
+package com.leeym.api.borderlessaccounts;
+
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import com.leeym.common.NumberId;
+
+import java.io.IOException;
+
+@JsonAdapter(BankDetailsId.TypeAdapter.class)
+public class BankDetailsId extends NumberId {
+    public BankDetailsId(String value) {
+        super(value);
+    }
+
+    static class TypeAdapter extends com.google.gson.TypeAdapter<BankDetailsId> {
+
+        @Override
+        public void write(JsonWriter out, BankDetailsId value) throws IOException {
+            out.value(value.toString());
+        }
+
+        @Override
+        public BankDetailsId read(JsonReader in) throws IOException {
+            return new BankDetailsId(in.nextString());
+        }
+    }
+}
---
+++ src/main/java/com/leeym/common/BaseUrl.java
@@ -0,0 +1,24 @@
+package com.leeym.common;
+
+import static com.leeym.common.Stage.LIVE;
+import static com.leeym.common.Stage.SANDBOX;
+
+public class BaseUrl extends FormattedString {
+    public static final BaseUrl SANDBOX_BASEURL = new BaseUrl("https://api.sandbox.transferwise.tech", SANDBOX);
+    public static final BaseUrl LIVE_BASEURL = new BaseUrl("https://api.transferwise.com", LIVE);
+
+    Stage stage;
+    public BaseUrl(String value, Stage stage) {
+        super(value);
+        this.stage = stage;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    @Override
+    public String format() {
+        return "^https://api.(sandbox.transferwise.tech|transferwise.com)$";
+    }
+}
