cf14c36f2e23050681580bf72bb6fa8e9160f71f
Yen-Ming Lee <leeym@leeym.com>
2020-07-04 01:03:10 -0700

    replace createQuote with buyTargetFromSource and sellSourceToTarget

--- src/main/java/com/leeym/api/quotes/QuotesApi.java
+++ src/main/java/com/leeym/api/quotes/QuotesApi.java
@@ -1,8 +1,14 @@
 package com.leeym.api.quotes;
 
 import com.leeym.api.BaseApi;
+import com.leeym.api.userprofiles.ProfileId;
 import com.leeym.common.ApiToken;
 import com.leeym.common.BaseUrl;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static com.leeym.api.quotes.Type.BALANCE_CONVERSION;
 
 // https://api-docs.transferwise.com/#quotes
 public class QuotesApi extends BaseApi {
@@ -11,8 +17,16 @@ public class QuotesApi extends BaseApi {
     }
 
     // https://api-docs.transferwise.com/#quotes-create
-    public QuoteResponse createQuote(QuoteRequest request) {
+    private QuoteResponse createQuote(QuoteRequest request) {
         String json = post("/v1/quotes", request);
         return gson.fromJson(json, QuoteResponse.class);
+    }
+
+    public QuoteResponse buyTargetFromSource(ProfileId profileId, Currency source, Currency target, BigDecimal value) {
+        return createQuote(new QuoteRequest(profileId, source, target, value, BALANCE_CONVERSION));
+    }
+
+    public QuoteResponse sellSourceToTarget(ProfileId profileId, Currency source, BigDecimal value, Currency target) {
+        return createQuote(new QuoteRequest(profileId, source, value, target, BALANCE_CONVERSION));
     }
 }
--- src/test/java/com/leeym/api/quotes/QuotesApiITCase.java
+++ src/test/java/com/leeym/api/quotes/QuotesApiITCase.java
@@ -16,14 +16,25 @@ class QuotesApiITCase extends BaseApiITCase {
     private final QuotesApi api = new QuotesApi(SANDBOX_BASEURL, SANDBOX_API_TOKEN);
 
     @Test
-    public void testCreateQuote() {
+    public void buyTargetFromSource() {
         BigDecimal amount = new BigDecimal("600");
-        QuoteRequest request = new QuoteRequest(SANDBOX_PERSONAL_PROFILE_ID, EUR, GBP, amount, BALANCE_CONVERSION);
-        QuoteResponse response = api.createQuote(request);
+        QuoteResponse response = api.buyTargetFromSource(SANDBOX_PERSONAL_PROFILE_ID, EUR, GBP, amount);
         assertEquals(SANDBOX_PERSONAL_PROFILE_ID, response.profile);
         assertEquals(EUR, response.source);
         assertEquals(GBP, response.target);
         assertEquals(amount.longValue(), response.targetAmount.longValue());
+        assertEquals(RateType.FIXED, response.rateType);
+        assertEquals(BALANCE_CONVERSION, response.type);
+    }
+
+    @Test
+    public void sellSourceToTarget() {
+        BigDecimal amount = new BigDecimal("600");
+        QuoteResponse response = api.sellSourceToTarget(SANDBOX_PERSONAL_PROFILE_ID, GBP, amount, EUR);
+        assertEquals(SANDBOX_PERSONAL_PROFILE_ID, response.profile);
+        assertEquals(GBP, response.source);
+        assertEquals(amount.longValue(), response.sourceAmount.longValue());
+        assertEquals(EUR, response.target);
         assertEquals(RateType.FIXED, response.rateType);
         assertEquals(BALANCE_CONVERSION, response.type);
     }
